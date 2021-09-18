7fd85fd776b8a127e8e396ee9c3c5d3e04e77fae
The-Gentleman <zraja4402@gmail.com>
2021-09-18 07:10:05 -0400

    created class with arrow functions

---
+++ hashPractice.js
@@ -0,0 +1,13 @@
+class HashTable {
+    setItem = (key, value) => {
+
+    }
+
+    getItem = (key) => {
+        return ''
+    }
+}
+
+const myTable = new HashTable()
+myTable.setItem('firstName' , 'spongeBob')
+myTable.getItem('firstName')
